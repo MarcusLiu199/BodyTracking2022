2b59a58ee1ad484574df2113e1e79830f7ae7d36
Marcus Liu <liu.mingku@northeastern.edu>
2023-08-09 16:47:10 -0400

    Initial Commit

---
+++ BodyTracking2022/BodyTracking2022App.swift
@@ -0,0 +1,17 @@
+//
+//  BodyTracking2022App.swift
+//  BodyTracking2022
+//
+//  Created by 柳铭坤 on 2023/8/9.
+//
+
+import SwiftUI
+
+@main
+struct BodyTracking2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
